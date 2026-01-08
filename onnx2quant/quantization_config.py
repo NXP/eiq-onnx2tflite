c30d5bbddc7a01d3f2dc5dec5dea78cc33694d2f
Jakub Kasem <jakub.kasem@nxp.com>
2026-01-08 17:19:29 +0100

    use random dataset flag; flag tests;

--- onnx2quant/quantization_config.py
+++ onnx2quant/quantization_config.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2024 NXP
+# Copyright 2024-2026 NXP
 #
 # License: LA_OPT_Online Code Hosting NXP_Software_License
 # See the LICENSE for more details.
@@ -8,6 +8,7 @@
 from onnxruntime.quantization import CalibrationDataReader
 
 from onnx2tflite.src import logger
+from typing import Literal
 
 PER_CHANNEL_DEFAULT = False
 
@@ -25,6 +26,7 @@ class QuantizationConfig:
         self.input_shapes_mapping: dict[str, tuple] | None = None
         self.allow_opset_10_and_lower: bool = False
         self.calibration_data_reader = calibration_data_reader
+        self.use_random_calibration_dataset: bool = False
         self.per_channel: bool = PER_CHANNEL_DEFAULT
         self.generate_artifacts_after_failed_shape_inference: bool = True
 
@@ -37,7 +39,7 @@ class QuantizationConfig:
                 if key in self.__dict__:
                     setattr(self, key, value)
 
-        logger.internal_assert(self.calibration_data_reader is not None, "Calibration data reader not provided!")
+        logger.internal_assert((self.calibration_data_reader is not None) ^ self.use_random_calibration_dataset, "Calibration data reader not provided!")
 
     def __repr__(self):
         attrs = []
